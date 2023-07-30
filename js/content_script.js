691e180fd1fdcf68b2b3ee3b9bed2337c476ffa1
Abdul Quddous <quddoux112@gmail.com>
2023-07-30 22:08:34 +0500

    first commit

---
+++ js/content_script.js
@@ -0,0 +1,293 @@
+function _0x2306(_0x4ac880, _0x474c57) {
+  const _0x23ad05 = _0x23ad();
+  return (
+    (_0x2306 = function (_0x23067a, _0x2f6e59) {
+      _0x23067a = _0x23067a - 0x78;
+      let _0x18e56a = _0x23ad05[_0x23067a];
+      return _0x18e56a;
+    }),
+    _0x2306(_0x4ac880, _0x474c57)
+  );
+}
+const _0xdb196b = _0x2306;
+(function (_0x39942b, _0x31944f) {
+  const _0x2bf061 = _0x2306,
+    _0x2dd855 = _0x39942b();
+  while (!![]) {
+    try {
+      const _0x24dec6 =
+        -parseInt(_0x2bf061(0x8c)) / 0x1 +
+        -parseInt(_0x2bf061(0x8e)) / 0x2 +
+        -parseInt(_0x2bf061(0xe1)) / 0x3 +
+        (-parseInt(_0x2bf061(0xd4)) / 0x4) * (parseInt(_0x2bf061(0x85)) / 0x5) +
+        (parseInt(_0x2bf061(0xc5)) / 0x6) * (parseInt(_0x2bf061(0x99)) / 0x7) +
+        (-parseInt(_0x2bf061(0xc1)) / 0x8) *
+          (-parseInt(_0x2bf061(0xbb)) / 0x9) +
+        (parseInt(_0x2bf061(0xa0)) / 0xa) * (parseInt(_0x2bf061(0x7a)) / 0xb);
+      if (_0x24dec6 === _0x31944f) break;
+      else _0x2dd855["push"](_0x2dd855["shift"]());
+    } catch (_0x41832a) {
+      _0x2dd855["push"](_0x2dd855["shift"]());
+    }
+  }
+})(_0x23ad, 0xd13d4),
+  chrome[_0xdb196b(0xd6)][_0xdb196b(0xa4)][_0xdb196b(0x89)](function (
+    _0x5517e4,
+    _0x517e7e,
+    _0x177b2b
+  ) {
+    const _0x1e9844 = _0xdb196b;
+    switch (_0x5517e4[_0x1e9844(0x9b)]) {
+      case _0x1e9844(0x9d):
+        fillForm()["then"](() => _0x177b2b({ message: !![] }));
+        break;
+      default:
+        break;
+    }
+    return !![];
+  });
+const fillForm = async () => {
+    const _0x159fc3 = _0xdb196b;
+    let _0x13f81f = document["createElement"]("div");
+    (_0x13f81f[_0x159fc3(0xcc)][_0x159fc3(0xcb)] = "fixed"),
+      (_0x13f81f["style"][_0x159fc3(0xa9)] = "0"),
+      (_0x13f81f["style"][_0x159fc3(0xbd)] = "0"),
+      (_0x13f81f[_0x159fc3(0xcc)][_0x159fc3(0xb2)] = _0x159fc3(0xc6)),
+      (_0x13f81f["style"][_0x159fc3(0x97)] = "100%"),
+      (_0x13f81f[_0x159fc3(0xcc)][_0x159fc3(0xdb)] = _0x159fc3(0x92)),
+      (_0x13f81f[_0x159fc3(0xcc)][_0x159fc3(0xa6)] = _0x159fc3(0xc7)),
+      (_0x13f81f[_0x159fc3(0xcc)]["display"] = _0x159fc3(0x79)),
+      (_0x13f81f[_0x159fc3(0xcc)]["justifyContent"] = "center"),
+      (_0x13f81f[_0x159fc3(0xcc)][_0x159fc3(0xac)] = "center"),
+      (_0x13f81f[_0x159fc3(0xcc)][_0x159fc3(0x93)] = _0x159fc3(0x9f)),
+      (_0x13f81f[_0x159fc3(0xcc)][_0x159fc3(0x9c)] = _0x159fc3(0x88)),
+      (_0x13f81f[_0x159fc3(0xda)] = _0x159fc3(0x8a)),
+      (_0x13f81f["id"] = _0x159fc3(0xb3)),
+      document[_0x159fc3(0xab)][_0x159fc3(0xc9)](_0x13f81f);
+    const { formData: _0x50cf55 } = await chrome[_0x159fc3(0xd0)]["sync"][
+        _0x159fc3(0xad)
+      ](_0x159fc3(0xd5)),
+      _0x2fee97 = document[_0x159fc3(0xbf)](_0x159fc3(0x7d)),
+      _0x5e8658 = document[_0x159fc3(0xbf)](_0x159fc3(0x8b)),
+      _0xf53507 = document["querySelector"](_0x159fc3(0xc8)),
+      _0x60622e = document[_0x159fc3(0xbf)](_0x159fc3(0xe3)),
+      _0x2eb5eb = document[_0x159fc3(0xbf)](_0x159fc3(0x7e)),
+      _0x4f31aa = document[_0x159fc3(0xbf)](_0x159fc3(0xde)),
+      _0x399c28 = document[_0x159fc3(0xbf)](_0x159fc3(0xbe)),
+      _0x5d560f = document[_0x159fc3(0xbf)](_0x159fc3(0x78)),
+      _0x4ce529 = document[_0x159fc3(0xbf)](_0x159fc3(0x98)),
+      _0x4aa299 = document["querySelector"](_0x159fc3(0xaa)),
+      _0x3a4e4e = document[_0x159fc3(0xbf)](_0x159fc3(0x9a)),
+      _0x10e713 = document["querySelector"](_0x159fc3(0xa7)),
+      _0x574a8f = document[_0x159fc3(0xbf)](_0x159fc3(0xa2)),
+      _0x33fc2e = document[_0x159fc3(0xbf)](_0x159fc3(0x7c)),
+      _0x55a010 = document[_0x159fc3(0xbf)](_0x159fc3(0x84)),
+      _0x4cf503 = document["querySelector"](_0x159fc3(0x80)),
+      _0x7808b4 = document[_0x159fc3(0xbf)](_0x159fc3(0xb0)),
+      _0x1ed83e = document[_0x159fc3(0xbf)](_0x159fc3(0x87)),
+      _0x745e5 = document[_0x159fc3(0xbf)](_0x159fc3(0x7b)),
+      _0x786407 = document[_0x159fc3(0xbf)](_0x159fc3(0xb8)),
+      _0x89c349 = document["querySelector"](_0x159fc3(0xb5)),
+      _0x3d38c4 = document["querySelector"](_0x159fc3(0x7f)),
+      _0x2b7228 = document[_0x159fc3(0xbf)](_0x159fc3(0xd2)),
+      _0x58996c = document["querySelector"]("#loom_type"),
+      _0x80ee84 = document[_0x159fc3(0xbf)](_0x159fc3(0x9e)),
+      _0xccdff2 = document[_0x159fc3(0xbf)]("#ornamentation"),
+      _0xabcc85 = document[_0x159fc3(0xbf)](_0x159fc3(0xba)),
+      _0x5316ed = document["querySelector"](_0x159fc3(0xb1)),
+      _0x13a1c8 = document[_0x159fc3(0xbf)]("#print_or_pattern_type"),
+      _0x5b3cc2 = document[_0x159fc3(0xbf)]("#comment"),
+      _0x2ca9c9 = document[_0x159fc3(0xbf)](_0x159fc3(0xb9));
+    return (
+      await fillValue(_0x13a1c8, _0x50cf55[_0x159fc3(0xd1)]),
+      await fillValue(_0x2fee97, _0x50cf55["Net\x20Weight\x20(gms)"]),
+      await fillValue(_0x5e8658, _0x50cf55[_0x159fc3(0x8d)]),
+      await fillValue(_0xf53507, _0x50cf55[_0x159fc3(0xd8)]),
+      await fillValue(_0x60622e, _0x50cf55[_0x159fc3(0xc3)]),
+      await fillValue(_0x2eb5eb, _0x50cf55[_0x159fc3(0xb7)]),
+      await fillValue(_0x399c28, _0x50cf55[_0x159fc3(0x94)]),
+      await fillValue(_0x5d560f, _0x50cf55[_0x159fc3(0xdf)]),
+      await fillValue(_0x4ce529, _0x50cf55[_0x159fc3(0x82)]),
+      await fillValue(_0x4aa299, _0x50cf55[_0x159fc3(0xa3)]),
+      await fillValue(_0x3a4e4e, _0x50cf55["Type"]),
+      await fillValue(_0x10e713, _0x50cf55[_0x159fc3(0xe0)]),
+      await fillValue(_0x574a8f, _0x50cf55[_0x159fc3(0xcf)]),
+      await fillValue(_0x33fc2e, _0x50cf55[_0x159fc3(0xbc)]),
+      await fillValue(_0x55a010, _0x50cf55["Meesho\x20Price"]),
+      await fillValue(
+        _0x4cf503,
+        _0x50cf55["Wrong/Defective\x20Returns\x20Price"]
+      ),
+      await fillValue(_0x7808b4, _0x50cf55[_0x159fc3(0x83)]),
+      await fillValue(_0x1ed83e, _0x50cf55[_0x159fc3(0xa1)]),
+      await fillValue(_0x745e5, _0x50cf55[_0x159fc3(0x96)]),
+      await fillValue(_0x89c349, _0x50cf55[_0x159fc3(0x86)]),
+      await fillValue(_0x3d38c4, _0x50cf55[_0x159fc3(0x81)]),
+      await fillValue(_0x2b7228, _0x50cf55[_0x159fc3(0xdc)]),
+      await fillValue(_0x58996c, _0x50cf55[_0x159fc3(0xd3)]),
+      await fillValue(_0x80ee84, _0x50cf55["Occasion"]),
+      await fillValue(_0xccdff2, _0x50cf55[_0x159fc3(0xcd)]),
+      await fillValue(_0xabcc85, _0x50cf55[_0x159fc3(0xca)]),
+      await fillValue(_0x5b3cc2, _0x50cf55[_0x159fc3(0xa5)]),
+      await fillValue(_0x2ca9c9, _0x50cf55[_0x159fc3(0x8f)]),
+      await fillValue(_0x786407, _0x50cf55["Blouse\x20Color"]),
+      await fillValue(_0x4f31aa, _0x50cf55["Border"]),
+      await fillValue(_0x5316ed, _0x50cf55["Pattern"]),
+      document[_0x159fc3(0xab)][_0x159fc3(0xb6)](_0x13f81f),
+      !![]
+    );
+  },
+  fillValue = async (_0x5ad332, _0x24ac6a) => {
+    const _0x5900e2 = _0xdb196b;
+    if (_0x5ad332) {
+      console[_0x5900e2(0xdd)](_0x5ad332["id"], _0x24ac6a);
+      if (
+        _0x5ad332[_0x5900e2(0x90)] === "INPUT" &&
+        _0x5ad332[_0x5900e2(0xa8)]
+      ) {
+        const _0xc5f1e4 = _0x5ad332[_0x5900e2(0x91)][_0x5900e2(0xbf)](
+          _0x5900e2(0x95)
+        );
+        _0xc5f1e4["dispatchEvent"](new MouseEvent("click", { bubbles: !![] })),
+          await new Promise((_0x9b0cac) => setTimeout(_0x9b0cac, 0xa));
+        const _0x397644 = Array[_0x5900e2(0xae)](
+          document["querySelectorAll"](_0x5900e2(0xe2))
+        )[_0x5900e2(0xb4)](
+          (_0x2fd8bb) =>
+            _0x2fd8bb["textContent"] === _0x24ac6a[_0x5900e2(0xce)]()
+        );
+        if (!_0x397644) {
+          console[_0x5900e2(0xc2)]("Option\x20not\x20found:\x20" + _0x24ac6a);
+          return;
+        }
+        _0x397644[_0x5900e2(0xaf)](new MouseEvent("click", { bubbles: !![] })),
+          await new Promise((_0x1de1fe) => setTimeout(_0x1de1fe, 0xa)),
+          _0xc5f1e4[_0x5900e2(0xaf)](
+            new MouseEvent(_0x5900e2(0xd9), { bubbles: !![] })
+          ),
+          _0x5ad332[_0x5900e2(0xaf)](
+            new Event(_0x5900e2(0xc4), { bubbles: !![] })
+          );
+      } else {
+        _0x5ad332[_0x5900e2(0xc0)] = _0x24ac6a;
+        var _0x4f5dab = new Event(_0x5900e2(0xd7), { bubbles: !![] });
+        _0x5ad332[_0x5900e2(0xaf)](_0x4f5dab);
+      }
+    }
+  };
+function _0x23ad() {
+  const _0x3a3dc2 = [
+    "#product_mrp",
+    "#pattern",
+    "width",
+    "loader",
+    "find",
+    "#blouse_pattern",
+    "removeChild",
+    "Blouse\x20Fabric",
+    "#blouse_color",
+    "#importer_details",
+    "#pallu_details",
+    "362556TksVIx",
+    "Packer\x20Details",
+    "left",
+    "#color",
+    "querySelector",
+    "value",
+    "136ObYDCy",
+    "error",
+    "Blouse",
+    "blur",
+    "33780SIIKNE",
+    "100%",
+    "10000",
+    "#product_name",
+    "appendChild",
+    "Pallu\x20Details",
+    "position",
+    "style",
+    "Ornamentation",
+    "toString",
+    "Manufacturer\x20Details",
+    "storage",
+    "Print\x20or\x20Pattern\x20Type",
+    "#brand",
+    "Loom\x20Type",
+    "11420BBnnHF",
+    "formData",
+    "runtime",
+    "change",
+    "Product\x20Name",
+    "click",
+    "innerText",
+    "backgroundColor",
+    "Brand",
+    "log",
+    "#border",
+    "Net\x20Quantity\x20(N)",
+    "COUNTRY\x20OF\x20ORIGIN",
+    "1808871JNfFIs",
+    ".MuiMenuItem-root",
+    "#blouse",
+    "#multipack",
+    "flex",
+    "1258961FYyEdh",
+    "#hsn_code",
+    "#packer_details",
+    "#product_weight_in_gms",
+    "#blouse_fabric",
+    "#border_width",
+    "#only_wrong_return_price",
+    "Border\x20Width",
+    "Saree\x20Fabric",
+    "MRP",
+    "#meesho_price",
+    "685GgGxoF",
+    "Blouse\x20Pattern",
+    "#supplier_gst_percent",
+    "20px",
+    "addListener",
+    "Loading...",
+    "#supplier_product_id",
+    "973467oECVgW",
+    "Style\x20code/\x20Product\x20ID\x20(optional)",
+    "354790CMqUDa",
+    "Importer\x20Details",
+    "nodeName",
+    "parentNode",
+    "rgba(0,\x200,\x200,\x200.7)",
+    "color",
+    "Color",
+    "svg",
+    "HSN\x20Code",
+    "height",
+    "#saree_fabric",
+    "35hKWhtY",
+    "#type",
+    "event",
+    "fontSize",
+    "fill_form",
+    "#occasion",
+    "white",
+    "200kfYoxM",
+    "GST",
+    "#manufacturer_details",
+    "Transparency",
+    "onMessage",
+    "Description",
+    "zIndex",
+    "#country_of_origin",
+    "readOnly",
+    "top",
+    "#transparency",
+    "body",
+    "alignItems",
+    "get",
+    "from",
+    "dispatchEvent",
+  ];
+  _0x23ad = function () {
+    return _0x3a3dc2;
+  };
+  return _0x23ad();
+}
---
+++ js/popup.js
@@ -0,0 +1,257 @@
+const _0x151c96 = _0x985f;
+function _0x985f(_0x2048dd, _0x1b4577) {
+  const _0x53d465 = _0x53d4();
+  return (
+    (_0x985f = function (_0x985f8, _0x441e0c) {
+      _0x985f8 = _0x985f8 - 0x185;
+      let _0x456812 = _0x53d465[_0x985f8];
+      return _0x456812;
+    }),
+    _0x985f(_0x2048dd, _0x1b4577)
+  );
+}
+(function (_0x1144ee, _0x597fcf) {
+  const _0x489de0 = _0x985f,
+    _0x1d8ecd = _0x1144ee();
+  while (!![]) {
+    try {
+      const _0x423b22 =
+        (-parseInt(_0x489de0(0x191)) / 0x1) *
+          (parseInt(_0x489de0(0x1b8)) / 0x2) +
+        parseInt(_0x489de0(0x1a2)) / 0x3 +
+        (-parseInt(_0x489de0(0x19d)) / 0x4) *
+          (parseInt(_0x489de0(0x1b0)) / 0x5) +
+        parseInt(_0x489de0(0x1c4)) / 0x6 +
+        (parseInt(_0x489de0(0x199)) / 0x7) *
+          (-parseInt(_0x489de0(0x1cd)) / 0x8) +
+        (parseInt(_0x489de0(0x1a4)) / 0x9) *
+          (parseInt(_0x489de0(0x1c1)) / 0xa) +
+        -parseInt(_0x489de0(0x1c0)) / 0xb;
+      if (_0x423b22 === _0x597fcf) break;
+      else _0x1d8ecd["push"](_0x1d8ecd["shift"]());
+    } catch (_0x5e0456) {
+      _0x1d8ecd["push"](_0x1d8ecd["shift"]());
+    }
+  }
+})(_0x53d4, 0x3dc28);
+const fileInputStyles = {
+    display: _0x151c96(0x1ab),
+    alignItems: _0x151c96(0x1ba),
+    gap: _0x151c96(0x18e),
+    cursor: _0x151c96(0x1be),
+    padding: _0x151c96(0x18e),
+    border: _0x151c96(0x1b1),
+    borderRadius: _0x151c96(0x195),
+    backgroundColor: "#f9f9f9",
+    color: _0x151c96(0x1a3),
+    marginBottom: "10px",
+  },
+  buttonStyles = {
+    padding: _0x151c96(0x19c),
+    borderRadius: _0x151c96(0x195),
+    backgroundColor: "#049dde",
+    color: _0x151c96(0x1ae),
+    border: _0x151c96(0x1a7),
+    cursor: _0x151c96(0x1be),
+    fontSize: _0x151c96(0x1c5),
+    fontWeight: _0x151c96(0x1b9),
+    width: "100%",
+  },
+  fileTextStyles = {
+    flex: 0x1,
+    whiteSpace: _0x151c96(0x1b2),
+    overflow: _0x151c96(0x197),
+    textOverflow: "ellipsis",
+  },
+  fileInputHiddenStyles = { display: _0x151c96(0x1a7) };
+function fetchFormValues() {
+  const _0x4c0f22 = _0x151c96;
+  chrome[_0x4c0f22(0x1bd)][_0x4c0f22(0x1bb)][_0x4c0f22(0x190)](
+    _0x4c0f22(0x18b),
+    function (_0x5cdc9c) {
+      const _0xc389b9 = _0x4c0f22,
+        _0x12e09c = _0x5cdc9c[_0xc389b9(0x18b)] || null,
+        _0x19da7b = document[_0xc389b9(0x1a8)]("form-details");
+      if (_0x12e09c) {
+        let _0x1aed68 = "";
+        _0x1aed68 += "<h3>Form\x20Data</h3>";
+        for (let _0x391d16 in _0x12e09c) {
+          _0x12e09c[_0xc389b9(0x1a6)](_0x391d16) &&
+            (_0x1aed68 +=
+              _0xc389b9(0x1aa) +
+              _0x391d16 +
+              ":\x20" +
+              _0x12e09c[_0x391d16] +
+              _0xc389b9(0x1b7));
+        }
+        _0x19da7b[_0xc389b9(0x19a)] = _0x1aed68;
+      } else _0x19da7b[_0xc389b9(0x188)] = _0xc389b9(0x1c9);
+    }
+  );
+}
+function _0x53d4() {
+  const _0x22ddcf = [
+    "bold",
+    "center",
+    "sync",
+    "fill_form",
+    "storage",
+    "pointer",
+    "log",
+    "477664MnIIFE",
+    "1727220tkINwJ",
+    "readAsBinaryString",
+    "sendMessage",
+    "2205936kFgNDx",
+    "16px",
+    "file-input",
+    "file",
+    "name",
+    "No\x20form\x20data\x20found,\x20Please\x20Upload\x20An\x20Excel\x20file",
+    "tabs",
+    "assign",
+    "DOMContentLoaded",
+    "600IzYkuH",
+    "file-label",
+    "SheetNames",
+    "addEventListener",
+    "textContent",
+    "utils",
+    "read",
+    "formData",
+    "createElement",
+    "form-details",
+    "8px",
+    "color",
+    "get",
+    "1gmkASJ",
+    "style",
+    "click",
+    "query",
+    "4px",
+    "button",
+    "hidden",
+    "binary",
+    "5999IDufEB",
+    "innerHTML",
+    "Choose\x20Excel\x20File",
+    "8px\x2016px",
+    "11596xBgMwx",
+    ".xlsx,\x20.xls",
+    "file-text",
+    "Sheets",
+    "result",
+    "281058yYltFD",
+    "#555",
+    "9fTdMlH",
+    "input",
+    "hasOwnProperty",
+    "none",
+    "getElementById",
+    "appendChild",
+    "<p>",
+    "flex",
+    "target",
+    "label",
+    "#ffffff",
+    "preventDefault",
+    "205VsdZhH",
+    "1px\x20solid\x20#ccc",
+    "nowrap",
+    "excelFileBtn",
+    "popup-content",
+    "className",
+    "files",
+    "</p>",
+    "309076AFVYFm",
+  ];
+  _0x53d4 = function () {
+    return _0x22ddcf;
+  };
+  return _0x53d4();
+}
+function handleFileChange(_0x5d7d4d) {
+  const _0x5af81b = _0x151c96;
+  _0x5d7d4d[_0x5af81b(0x1af)]();
+  const _0x414333 = _0x5d7d4d["target"][_0x5af81b(0x1b6)][0x0];
+  if (_0x414333) {
+    const _0x28459a = new FileReader();
+    (_0x28459a["onload"] = function (_0x4af035) {
+      const _0x7961e8 = _0x5af81b,
+        _0x4035eb = _0x4af035[_0x7961e8(0x1ac)][_0x7961e8(0x1a1)],
+        _0x9272e2 = XLSX[_0x7961e8(0x18a)](_0x4035eb, {
+          type: _0x7961e8(0x198),
+        }),
+        _0x3ea08e = _0x9272e2[_0x7961e8(0x186)][0x0],
+        _0xf60448 = _0x9272e2[_0x7961e8(0x1a0)][_0x3ea08e],
+        _0x505d13 = XLSX[_0x7961e8(0x189)]["sheet_to_json"](_0xf60448, {
+          header: 0x1,
+          defval: "",
+        }),
+        _0x4581ea = {};
+      _0x505d13["forEach"]((_0x30c548) => {
+        _0x4581ea[_0x30c548[0x0]] = _0x30c548[0x1];
+      }),
+        chrome[_0x7961e8(0x1bd)][_0x7961e8(0x1bb)]["set"]({
+          formData: _0x4581ea,
+        }),
+        fetchFormValues();
+    }),
+      _0x28459a[_0x5af81b(0x1c2)](_0x414333);
+  }
+}
+function fillForm() {
+  const _0x2798d0 = _0x151c96;
+  chrome[_0x2798d0(0x1ca)][_0x2798d0(0x194)](
+    { active: !![], currentWindow: !![] },
+    function (_0x253086) {
+      const _0x110a6e = _0x2798d0,
+        _0x3acfb8 = _0x253086[0x0];
+      _0x3acfb8["id"] &&
+        chrome[_0x110a6e(0x1ca)][_0x110a6e(0x1c3)](
+          _0x3acfb8["id"],
+          { event: _0x110a6e(0x1bc), tabId: _0x3acfb8["id"] },
+          (_0x36d5ee) => {
+            const _0x93cd4e = _0x110a6e;
+            console[_0x93cd4e(0x1bf)]("result\x20message:", _0x36d5ee);
+          }
+        );
+    }
+  );
+}
+document[_0x151c96(0x187)](_0x151c96(0x1cc), function () {
+  const _0x33027e = _0x151c96;
+  fetchFormValues();
+  const _0x43f243 = document[_0x33027e(0x18c)]("div");
+  (_0x43f243["id"] = _0x33027e(0x18d)),
+    (_0x43f243[_0x33027e(0x192)][_0x33027e(0x18f)] = _0x33027e(0x1a3));
+  const _0x1cb265 = document[_0x33027e(0x18c)](_0x33027e(0x1ad));
+  (_0x1cb265["htmlFor"] = _0x33027e(0x1b3)),
+    (_0x1cb265[_0x33027e(0x1b5)] = _0x33027e(0x185)),
+    Object[_0x33027e(0x1cb)](_0x1cb265[_0x33027e(0x192)], fileInputStyles);
+  const _0x801225 = document[_0x33027e(0x18c)]("span");
+  (_0x801225[_0x33027e(0x1b5)] = _0x33027e(0x19f)),
+    Object[_0x33027e(0x1cb)](_0x801225[_0x33027e(0x192)], fileTextStyles),
+    (_0x801225[_0x33027e(0x188)] = _0x33027e(0x19b));
+  const _0x1b08ac = document["createElement"](_0x33027e(0x1a5));
+  (_0x1b08ac["type"] = _0x33027e(0x1c7)),
+    (_0x1b08ac[_0x33027e(0x1c8)] = "excelFile"),
+    (_0x1b08ac["id"] = _0x33027e(0x1b3)),
+    (_0x1b08ac["accept"] = _0x33027e(0x19e)),
+    (_0x1b08ac[_0x33027e(0x1b5)] = _0x33027e(0x1c6)),
+    Object[_0x33027e(0x1cb)](
+      _0x1b08ac[_0x33027e(0x192)],
+      fileInputHiddenStyles
+    ),
+    _0x1b08ac[_0x33027e(0x187)]("change", handleFileChange),
+    _0x1cb265["appendChild"](_0x801225),
+    _0x1cb265[_0x33027e(0x1a9)](_0x1b08ac);
+  const _0x1cc542 = document["createElement"](_0x33027e(0x196));
+  (_0x1cc542[_0x33027e(0x188)] = "Fill\x20the\x20Form"),
+    _0x1cc542[_0x33027e(0x187)](_0x33027e(0x193), fillForm),
+    Object[_0x33027e(0x1cb)](_0x1cc542[_0x33027e(0x192)], buttonStyles);
+  const _0x42a68c = document[_0x33027e(0x1a8)](_0x33027e(0x1b4));
+  _0x42a68c["appendChild"](_0x1cb265),
+    _0x42a68c[_0x33027e(0x1a9)](_0x1cc542),
+    _0x42a68c[_0x33027e(0x1a9)](_0x43f243);
+});
