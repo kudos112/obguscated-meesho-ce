543ed543c573d1927d0f0550d97e538b3e791d29
Abdul Quddous <quddoux112@gmail.com>
2023-08-03 04:32:20 +0500

    added icon click

---
+++ js/service_worker.js
@@ -0,0 +1,62 @@
+var _0x26b2cd = _0x4c8a;
+function _0x4c8a(_0x47d0dd, _0xa0b2ea) {
+  var _0x54257a = _0x5425();
+  return (
+    (_0x4c8a = function (_0x4c8a3a, _0x328e65) {
+      _0x4c8a3a = _0x4c8a3a - 0x104;
+      var _0x503d30 = _0x54257a[_0x4c8a3a];
+      return _0x503d30;
+    }),
+    _0x4c8a(_0x47d0dd, _0xa0b2ea)
+  );
+}
+(function (_0x2b6d2e, _0x1e3511) {
+  var _0x48ec22 = _0x4c8a,
+    _0x2ffa4e = _0x2b6d2e();
+  while (!![]) {
+    try {
+      var _0x6e490a =
+        (-parseInt(_0x48ec22(0x108)) / 0x1) *
+          (-parseInt(_0x48ec22(0x10d)) / 0x2) +
+        parseInt(_0x48ec22(0x10a)) / 0x3 +
+        parseInt(_0x48ec22(0x104)) / 0x4 +
+        (parseInt(_0x48ec22(0x10c)) / 0x5) *
+          (parseInt(_0x48ec22(0x10b)) / 0x6) +
+        (-parseInt(_0x48ec22(0x110)) / 0x7) *
+          (-parseInt(_0x48ec22(0x109)) / 0x8) +
+        (-parseInt(_0x48ec22(0x10f)) / 0x9) *
+          (parseInt(_0x48ec22(0x105)) / 0xa) +
+        (-parseInt(_0x48ec22(0x106)) / 0xb) *
+          (parseInt(_0x48ec22(0x107)) / 0xc);
+      if (_0x6e490a === _0x1e3511) break;
+      else _0x2ffa4e["push"](_0x2ffa4e["shift"]());
+    } catch (_0x577946) {
+      _0x2ffa4e["push"](_0x2ffa4e["shift"]());
+    }
+  }
+})(_0x5425, 0x198d5),
+  chrome["sidePanel"]
+    [_0x26b2cd(0x111)]({ openPanelOnActionClick: !![] })
+    ["catch"]((_0xc81d14) => console[_0x26b2cd(0x10e)](_0xc81d14));
+function _0x5425() {
+  var _0xce9cca = [
+    "setPanelBehavior",
+    "431740NFIwGv",
+    "26020IrRdSW",
+    "1298MaGAKh",
+    "38352oNFhFr",
+    "1LOjsqx",
+    "5192VKsMvZ",
+    "546888vMqDgK",
+    "221202uHzesP",
+    "10cFsOow",
+    "216214lKODpF",
+    "error",
+    "27uiJcQf",
+    "189QxqxWM",
+  ];
+  _0x5425 = function () {
+    return _0xce9cca;
+  };
+  return _0x5425();
+}
